1c9d429b4532574862fe5d216901b48dc8f29868
Ken Hileman <ken.hileman@outlook.com>
2024-04-11 20:25:21 -0400

    fixed typo

--- factors.py
+++ factors.py
@@ -4,7 +4,7 @@
 # NOTE: No ReadMe Using other IDE
 # Description: Asks the user to enter a positive integer and then prints a list of all positive integers that divide that number evenly including itself, in ascending order.
 
-num = int(input("Please enter a positve integer: "))
+num = int(input("Please enter a positive integer: "))
 
 print("The factors of", num, "are:")
 
