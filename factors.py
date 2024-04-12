7fdc07d1e014532ef94ce42c14015f71e8e4e3a7
Ken Hileman <ken.hileman@outlook.com>
2024-04-11 20:18:33 -0400

    Assignment 3b First Commit

---
+++ factors.py
@@ -0,0 +1,11 @@
+# Author: Kenneth Hileman
+# GitHub username: PHTIWringer
+# Date: 04/09/2024
+# NOTE: No ReadMe Using other IDE
+# Description: Asks the user to enter a positive integer and then prints a list of all positive integers that divide that number evenly including itself, in ascending order.
+
+num = int(input("Please enter a positve integer: "))
+
+for i in range(1, num + 1):
+        if num % i == 0:
+            print(i)
